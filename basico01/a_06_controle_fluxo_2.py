46e580e0af92b6d3026e48aa308b7fee60515667
Artur Dantas <arturd_96@hotmail.com>
2023-08-25 12:29:20 -0300

    fundamentos python controle fluxo 2

---
+++ basico01/a_06_controle_fluxo_2.py
@@ -0,0 +1,19 @@
+import requests
+from bs4 import BeautifulSoup
+from tinydb import TinyDB, Query
+
+
+def main():
+    construir_url()
+
+def construir_url():
+    links_notasdeimprensa="https://www.gov.br/mre/pt-br/canais_atendimento/imprensa/notas-a-imprensa?b_start:int="
+    contador = 360
+    while contador >= 0:
+        url_completa = links_notasdeimprensa + str(contador)
+        contador = contador - 30
+        print(url_completa)
+
+
+if __name__ == "__main__":
+    main()
