566778994c1ab1621b7ec3ad0541729eac687a1c
Artur Dantas <arturd_96@hotmail.com>
2023-09-04 10:11:12 -0300

    teste2

---
+++ basico01/teste2.py
@@ -0,0 +1,14 @@
+from math import sqrt
+
+'''
+import "biblioteca" - para importar a biblioteca toda
+from "biblioteca" import "algo específico daquela biblioteca" - para quando eu não quero a biblioteca toda, só algo específico dela
+por exemplo: comandos matemáticos como sqrt, factorial etc na biblioteca 'math'
+
+verificar no google as bibliotecas e comandos disponíveis para python
+'''
+
+num = 4
+raiz = sqrt(num)
+print('A raíz de', (num), 'é igual a', (raiz))
+
