cffb410542441ab1f796529d846c23e4dc0989b3
Artur Dantas <arturd_96@hotmail.com>
2023-09-05 10:04:30 -0300

    testes em tuplas fix #2

--- basico01/a_04_tipos_avancados.py
+++ basico01/a_04_tipos_avancados.py
@@ -2,6 +2,21 @@ def main():
     inserir="Brasil"
     lista(b=inserir)
 
+def tupla():
+    tupla = ('a', 'b', 'c', 'd')   #com parênteses ou não; recomendável usar parênteses
+    x = tupla.count('c')  #conta quantos desse caracter específico tem na tupla
+    y = tupla.index('d')  #posição do caracter na tupla
+    if 'c' in tupla:
+        print('"c" está na tupla')
+    elif 'b' in tupla:   #não foi executado quando if verdadeiro; foi executado quando if falso. Dois 'ifs' são executados de qualquer forma, 'elif' é executado quando 'if' é falso.
+            print('"b" está na tupla')
+   # if not 'e' in tupla:
+       # print('"e" não está na tupla')
+    print('a' in tupla)  #True = existe na tupla. False = não existe na tupla
+    print(tupla)
+    print(x)
+    print(y)
+
 def lista(b,a="Argentina"):
     lista=[]
     print(b)
@@ -28,7 +43,7 @@ def dicionário():
 
 
 if __name__ == "__main__":
-    dicionário()
+    tupla()
 
 # len = contagem
 # append = adicionar
