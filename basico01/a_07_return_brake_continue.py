5826060d37ffb0b7c8a83e5fe7e196cc4a252ab8
Artur Dantas <arturd_96@hotmail.com>
2023-09-06 12:32:15 -0300

    lista TODO e captação do html de notas de imprensa mre

--- basico01/a_07_return_brake_continue.py
+++ basico01/a_07_return_brake_continue.py
@@ -7,16 +7,47 @@ from tinydb import TinyDB, Query
 # TODO: acessar página através da função "acessar_página"
 # TODO: printar os nomes de todos os títulos das 3 primeiras páginas das notas de imprensa
 
-def acessar_página(url):
+def acessar_pagina(url):
     página = requests.get(url)
     bs = BeautifulSoup(página.text, "html.parser")
     # print(bs)
     return bs
 
+def extrair_infos():
+    lista_de_links = construir_url()[0]
+ #   print(lista_de_links)
+    for link_geral in lista_de_links:
+        print(link_geral)
+        html = acessar_pagina(link_geral)
+        #print(html)
+        #<div id="content-core">
+        conteudo = html.find('div', attrs={'id':'content-core'})
+        lista_nota_imprensa = conteudo.find_all("article")
+        for nota_de_imprensa in lista_nota_imprensa:
+            titulo = nota_de_imprensa.h2.text.strip()
+            link = nota_de_imprensa.a['href']
+            numero = nota_de_imprensa.span.text
+            lista_datas = nota_de_imprensa.find_all('span', attrs={'class':'summary-view-icon'})
+            data = lista_datas[0].text.strip()
+            horário = lista_datas[1].text.strip()
+            print(titulo)
+            print(link)
+            print(numero)
+            print(data)
+            print(horário)
+            print("###")
+
+
+    # listaurls = tupla[0]
+    # print(listaurls)
+    # labri = tupla[1]
+    # print(labri)
+
 def construir_url():
     links_notasdeimprensa="https://www.gov.br/mre/pt-br/canais_atendimento/imprensa/notas-a-imprensa?b_start:int="
     contador = 360
     listaurls = []
+    labri = 'artur'
     while contador >= 0:
         url_completa = links_notasdeimprensa + str(contador)
         if contador == 0:
@@ -24,16 +55,15 @@ def construir_url():
         contador = contador - 30
         # print(url_completa)
         listaurls.append(url_completa)
-    return listaurls
-    print(listaurls)
+    #print(listaurls)
+    return listaurls, labri
 
 def main():
     url = "https://www.gov.br/mre/pt-br/canais_atendimento/imprensa/notas-a-imprensa"
     # página_web = acessar_página(url)
-    lista_urls = construir_url()
-    print(lista_urls)
+    lista = extrair_infos()
+
 
 
 if __name__ == "__main__":
     main()
-
